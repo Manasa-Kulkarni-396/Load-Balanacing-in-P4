cc617724f850bb4ab2ccb544bca66adb2cf3ef7b
Manasa-Kulkarni-396 <100560216+Manasa-Kulkarni-396@users.noreply.github.com>
2022-03-01 19:54:24 +0530

    Create cmd_add.py

---
+++ DPDK/cmd_add.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('sudo /home/user/behavioral-model/targets/simple_switch/simple_switch_CLI --thrift-port=9090 < cmd.txt')
+os.system('sudo /home/user/behavioral-model/targets/simple_switch/simple_switch_CLI --thrift-port=9091 < cmd1.txt')
+os.system('sudo /home/user/behavioral-model/targets/simple_switch/simple_switch_CLI --thrift-port=9092 < cmd2.txt')
